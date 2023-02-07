0e2e01adf73afc2db1471916eb4b41529fba4e75
Lucas Golden <118189246+solo-lgolden@users.noreply.github.com>
2023-02-07 14:57:18 -0500

    init

--- bin/dart_command_line.dart
+++ bin/dart_command_line.dart
@@ -1,9 +1,38 @@
 import 'dart:io';
 
-void main(List<String> arguments) {
-  Process.run('tmux', []);
+Map<String, String> environmentVariables = {};
+
+void main(List<String> arguments) async {
+  environmentVariables = Platform.environment;
+  await installDependencies();
+  await createAndInstallEmulator();
+  await launchAppium();
+  await runTests();
+}
+
+Future<void> launchAppium() async {
+  print('Executing tmux and appium');
+  await Process.run('tmux', []);
   Future.delayed(Duration(seconds: 1), (){
     Process.run('appium', []);
   });
-  Process.run('tmux', ['detach']);
+  await Process.run('tmux', ['detach']);
+}
+
+Future<void> installDependencies()async{
+  print('Installing dependencies');
+  await Process.run("pip", ["install", "-r", "bin/requirements.txt"]);
+  await Process.run("brew", ["install", "tmux"]);
+}
+
+Future<void> createAndInstallEmulator()async{
+  print('Installing Emulator');
+  await Process.run("android", ["create avd", "-n my_avd", '-t 29']);
+  await Process.run("emulator", ["-avd my_avd"]);
+  await Process.run("adb", ["install ${environmentVariables['AWS_APK_PATH']}"]);
+}
+
+Future<void> runTests()async{
+  print('Running tests');
+  await Process.run('robot', ["solo_appbeta_e2e.robot"]);
 }
